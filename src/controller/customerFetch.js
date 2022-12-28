00465556908e1c5c05074da6f3d78437295b82b7
freelancerliketodev <freelancerliketodev@gmail.com>
2022-12-28 09:27:52 +0400

    change

--- src/controller/customerFetch.js
+++ src/controller/customerFetch.js
@@ -3,25 +3,33 @@ import axios from "axios";
 
 
 export default async function customerFetch(req, res) {
-  const data  = req.body;
-  const customerId = data.customer.id
-  const config = {
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json;charset=UTF-8',
-      "X-Shopify-Access-Token": `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`,
-    }
-  }
+  const customerdata  = req.body ;
+  console.log('customerdata :', customerdata);
 
-  try {
-    const response =  await axios.put(`https://the-elegant-fashion-boutique.myshopify.com/admin/api/2022-04/customers/${customerId}.json`,data,config)
-    if(response.status === 200){
-      res.json(response.data)
-    }
-  } 
-  catch (error) {
-    console.log(error.message)
-    res.send(error)
-  }
+
+  const customerId = req.body?.customer?.id
+  let headersList = {
+    "Accept": "*/*",
+    "Content-Type": "application/json",
+    "X-Shopify-Access-Token": "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" 
+   }
+   let bodyContent = JSON.stringify(customerdata);
+
+   
+   let reqOptions = {
+     url: `https://infiniterushmore.com/admin/api/2022-04/customers/${customerId}.json`,
+     method: "PUT",
+     headers: headersList,
+     data: bodyContent,
+   }
+   
+   try {
+     let response = await axios.request(reqOptions);
+     res.json(response.data)
+    
+    
+   } catch (error) {
+      console.log(error)
+   }
   
 }
